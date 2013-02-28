8d553e66f95c06ce03a2a4e5babd2e126605a55b
Jeff Cohen <jeff@codeacademy.org>
2013-02-27 18:18:39 -0600

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ mileage_chart = {
 100.times do
   departure_code, arrival_code = airports.sample(2)
 
-  Flight.create number: "#{rand(100..999)}",
+  Flight.create number: "#{rand(900) + 100}",
       departure_airport: departure_code,
       departs_at: Time.now.beginning_of_day + rand(24.hours),
       arrival_airport: arrival_code,
